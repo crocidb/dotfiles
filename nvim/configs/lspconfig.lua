4e6d80e29241211e28a6d66fdd1a116c7e59feae
Bruno Croci <bruno@croci.me>
2026-04-08 00:21:40 +0200

    harper ls should only work on markdown and text files

--- nvim/configs/lspconfig.lua
+++ nvim/configs/lspconfig.lua
@@ -10,6 +10,9 @@ vim.lsp.enable("ty")
 vim.lsp.enable("gdscript")
 vim.lsp.enable("racket_langserver")
 vim.lsp.enable("glsl_analyzer")
+vim.lsp.config("harper_ls", {
+	filetypes = { "markdown", "text" },
+})
 vim.lsp.enable("harper_ls")
 
 -- Diagnostics
